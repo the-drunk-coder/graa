9da1001b3e4ae1dfb01adc0026fd4567c7011698
nik <nik@parkellipsen.de>
2015-01-09 20:53:32 +0100

    basic player mechanism workin

---
+++ GraphStructures.py
@@ -0,0 +1,178 @@
+# Import graphviz
+import sys, os, copy
+sys.path.append('..')
+sys.path.append('/usr/lib/graphviz/python/')
+sys.path.append('/usr/lib64/graphviz/python/')
+import graphviz
+
+from graphviz import Digraph
+
+class GraphError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+"""
+A node, consting of an
+"""
+class Node():
+    def __init__(self, node_id, node_content, meta=""):
+        self.id = node_id
+        self.content = node_content
+        # space for arbitrary meta information
+        self.meta = meta
+
+"""
+An edge, consisting of the destination node, the transition probability and the transition duration.
+
+If transition probability is None, it will be calculated when the edge is added.
+
+"""
+class Edge():
+    def __init__(self, destination_node_id, transition_duration, transition_probability=None):
+        self.dest = destination_node_id
+        self.prob = transition_probability
+        self.dur = transition_duration
+        
+class Graph():
+    def __init__(self):
+        self.nodes = {}
+        self.edges = {}
+        self.start_node_id = None
+        self.current_node_id = None
+    def add_node(self, node):
+        # by convention, first added node is start node
+        if self.start_node_id == None:
+            self.start_node_id = node.id
+            self.current_node_id = node.id
+        self.nodes[node.id] = node
+        self.edges[node.id] = []
+    # Add an edge, a a tuple of destination node and transition
+    def add_edge(self, source_node_id, new_edge):
+        if source_node_id not in self.nodes or new_edge.dest not in self.nodes:
+            raise GraphError("nodes for this edge not present")
+        else:
+            if new_edge.prob != None:
+                self.edges[source_node_id].append(new_edge)
+            else:
+                new_prob = int(100/(len(self.edges[source_node_id]) + 1))
+                for edge in self.edges[source_node_id]:
+                    edge.prob = new_prob
+                new_edge.prob = new_prob
+                self.edges[source_node_id].append(new_edge)                
+    def render(self, filename, render="content"):
+        dot = Digraph(comment="",edge_attr={'len': '6', 'weight':'0.00001'})
+        dot.engine = 'dot'
+        # add nodes to dot graph
+        for node_key in self.nodes.keys():
+            node_content = "nil"
+            # either use id or content to mark graph nodes
+            if render == "id":
+                    node_content = str(self.nodes[node_key].id)
+            elif render == "content":
+                if len(self.nodes[node_key].content) > 0:
+                    node_content = ', '.join(str(x) for x in self.nodes[node_key].content)
+            else:
+                node_content = str(self.nodes[node_key].id) + ":"
+                if len(self.nodes[node_key].content) > 0:
+                    node_content += ', '.join(str(x) for x in self.nodes[node_key].content) + ":"
+                else:
+                    node_content += "nil:"
+                if len(self.nodes[node_key].meta) > 0:
+                    node_content += self.nodes[node_key].meta
+                else:
+                    node_content += "nil"
+            dot.node(str(self.nodes[node_key].id), node_content)
+        #add edges to dot graph
+        for edge_key in self.edges.keys():
+            for edge in self.edges[edge_key]:
+                dot.edge(str(edge_key), str(edge.dest))
+        if not os.path.exists("graph"):
+            os.makedirs("graph")
+        dot.render("graph/" + filename + ".gv")
+
+class GraphTool():
+    # destructively reverse direction of edges 
+    def reverse_digraph(self, graph):
+        for i in range(0, len(graph.nodes)):
+            node = graph.nodes[i]
+            for edge in graph.edges[node.id]:
+                # don't touch self-referential edges ...
+                if edge.dest != node.id:
+                    rev_edge = Edge(node.id, edge.dur, edge.prob) 
+                    graph.add_edge(edge.dest, rev_edge)
+                    graph.edges[node.id].remove(edge)
+                    
+    
+class TraversalTool():
+    # returns node traversal, breadth first
+    def bf_trav(self, graph):
+        node_stack = []
+        nodes_unvisited = [x for x in range(0,len(graph.nodes) + 1)]
+        traversal_list = []
+        #remove start node and push to stack
+        nodes_unvisited.remove(0)
+        node_stack.append(0)
+        traversal_list.append(0)
+        while len(node_stack) != 0:
+            current_node = node_stack.pop()
+            for edge in graph.edges[current_node]:
+                if edge.dest in nodes_unvisited:
+                    nodes_unvisited.remove(edge.dest)
+                    node_stack.append(edge.dest)
+                    traversal_list.append(edge.dest)
+        return traversal_list
+    # return (dfs) topological sorting of the nodes
+    def topo_trav(self, graph):
+        dfs_tree = DfsTree(graph)
+        sorted_tuples = sorted(list(zip(dfs_tree.all_node_ids, dfs_tree.finish_time)), key=lambda time:time[1])
+        sorted_node_ids = [tpl[0] for tpl in sorted_tuples]
+        return list(reversed(sorted_node_ids))
+                           
+class DfsTree():
+    def __init__(self, graph):
+        self.graph = graph
+        self.node_stack = []
+        self.node_color = []
+        self.discovery_time =[]
+        self.finish_time = []
+        self.predecessor = []
+        self.time = 0
+        self.all_node_ids = []
+        for i in range(0, len(graph.nodes)):
+            self.node_color.append('white')
+            self.discovery_time.append(0)
+            self.finish_time.append(0)
+            self.predecessor.append(None)
+            self.all_node_ids.append(graph.nodes[i].id)
+        self.dfs()
+    def dfs(self):
+        for i in range(0, len(self.graph.nodes)):
+            if self.node_color[self.graph.nodes[i].id] == 'white':
+                self.dfs_visit(self.graph.nodes[i])
+    def dfs_visit(self, node):
+        # actual algorithm ..
+        self.node_color[node.id] = 'gray'
+        self.time += 1
+        self.discovery_time[node.id] = self.time
+        # the child nodes are directly stored as integers, so we can use them directly here 
+        for edge in self.graph.edges[node.id]:
+            if self.node_color[edge.dest] == 'white':
+                self.predecessor[edge.dest] = node.id
+                self.dfs_visit(self.graph.nodes[edge.dest])
+        self.node_color[node.id] = 'black'
+        self.time += 1
+        self.finish_time[node.id] = self.time
+    def print_results(self):
+        # generate result lists
+        finish = sorted(list(zip(self.all_node_ids, self.finish_time)), key=lambda time:time[1])
+        disc = list(zip(self.all_node_ids, self.discovery_time))
+        col = list(zip(self.all_node_ids, self.node_color))
+        pre = list(zip(self.all_node_ids, self.predecessor))
+        # print them
+        print("SORTED FINISH TIMES:" + str(finish))
+        print("DISCOVERY TIMES:" + str(disc))
+        print("COLORS:" + str(col))
+        print("PREDECESSORS:" + str(pre))
+ 
---
+++ shell.py
@@ -0,0 +1,204 @@
+#! /usr/bin/env python
+import cmd, readline, time, sched, threading, random
+from pyparsing import *
+from GraphStructures import *
+from queue import Queue
+
+# ideas:
+# node as function call, eval, node timing within node funciton
+# don't modifiy the durations at player-level, but only in the data structure!
+
+
+"""
+The Player class.
+
+Each graph lives in its own player, each player has its own thread.
+
+"""
+class GraaPlayer():   
+    def __init__(self, session, graph_id):
+        self.session = session
+        self.graph_id = graph_id
+        self.graph_thread = threading.Thread(target=self.start_play, args=(session, graph_id))
+        self.graph_thread.deamon = True
+        self.active = False
+    def start(self):
+        self.active = True
+        self.graph_thread.start()
+    def start_play(self, session, graph_id):
+        self.sched = sched.scheduler(time.monotonic, time.sleep)
+        self.play(session, graph_id)
+        self.sched.run()
+    def play(self, session, graph_id):
+        graph = session.graphs[graph_id]
+        current_node = graph.nodes[graph.current_node_id]
+        self.eval_node(current_node)
+        if(self.active):
+            self.sched_next_node(session, graph_id, graph.current_node_id)
+    # schedule next node
+    def sched_next_node(self, session, graph_id, current_node_id):
+        chosen_edge = self.choose_edge(session, graph_id, current_node_id)
+        edge = session.graphs[graph_id].edges[current_node_id][chosen_edge]
+        session.graphs[graph_id].current_node_id = edge.dest
+        if(self.active):
+            self.sched.enter(edge.dur / 1000, 1, self.play, argument=(session, graph_id))        
+    # choose edge for next transition
+    def choose_edge(self, session, graph_id, node_id):
+        weights = [edge.prob for edge in session.graphs[graph_id].edges[node_id]]
+        print(str(weights))
+        choice_list = []
+        for i, weight in zip(range(len(weights)), weights):
+           choice_list += [str(i)] * weight
+        return int(random.choice(choice_list))
+    # eval node content
+    def eval_node(self, node):
+        print("evaluating graph: {}, node id: {}, content: {}".format(self.graph_id, node.id, node.content))
+      
+"""
+The scheduler, taking care of the time and process spawning.
+
+"""
+class GraaScheduler():
+    def __init__(self, session):
+        self.graphs = {}
+        # LIFO Queue
+        self.graph_queue = Queue()
+        self.session = session
+        self.beat_thread = threading.Thread(target=self.start_beat, args=(session,))
+        self.beat_thread.deamon = True
+        self.beat_thread.start()
+    #def start_graph(self, graph_id):
+    def queue_graph(self, graph_id):
+        self.graph_queue.put(graph_id)
+        print("queuing graph with id: {}".format(graph_id))
+    # function to be called by scheduler in separate process
+    def start_beat(self, session):
+        self.sched = sched.scheduler(time.monotonic, time.sleep)
+        self.beat(session)
+        self.sched.run() 
+    def beat(self, session):
+        #print("beat, tempo: {}".format(session.tempo))
+        while not self.graph_queue.empty():
+            self.start_graph(session, self.graph_queue.get())
+        if(session.active):
+            self.sched.enter(60.0 / session.tempo, 1, self.beat, argument=(session,))
+    def start_graph(self, session, graph_id):
+        player = GraaPlayer(session, graph_id)
+        session.players[graph_id] = player
+        player.start()
+        
+        
+"""
+Contains all the graphs and some metadata in a session.
+
+"""
+class GraaSession():
+    def __init__(self):
+        self.graphs = {}
+        self.players = {}
+        self.tempo = 119
+        self.active = True
+    def add_node(self, node_tuple):
+        print("add node: " + str(node_tuple))
+        graph_id = node_tuple[0]
+        if graph_id not in self.graphs:
+            self.graphs[graph_id] = Graph()
+            print("initialized graph with id: \'" + graph_id + "\'")
+        self.graphs[graph_id].add_node(node_tuple[1])
+        print("added node with id \'{}\' to graph \'{}\'".format(node_tuple[1].id, graph_id))
+    def add_edge(self, edge_tuple):
+        graph_id = edge_tuple[0]
+        self.graphs[graph_id].add_edge(edge_tuple[1], edge_tuple[2])
+        print("added edge from node \'{}\' to node \'{}\'".format(edge_tuple[1],edge_tuple[2].dest))
+    
+"""
+Parse nodes and edges from the command line input 
+ 
+"""
+class GraaParser():
+    #grammar rules for node and edge lines
+    graph_id = Word(alphas)
+    node_id = graph_id + Word(nums)
+    node_type = Word(alphas)
+    node_line = node_id + ":" + node_type + OneOrMore(":" + Word(alphanums))
+    transition = Word(nums) + Optional(":" + Word(nums))
+    edge_line = node_id + "-" + transition + "->" + node_id
+    def parse_node(self, arg):
+        node_list = self.node_line.parseString(arg, parseAll=True)
+        graph_id = node_list[0]
+        node_id = node_list[1]
+        node_params = list(filter(lambda a: a != ":", node_list[2:]))
+        # create and return node
+        return (graph_id, Node(node_id, node_params))
+    def parse_edge(self, arg):
+        edge_list = self.edge_line.parseString(arg, parseAll=True)
+        graph_id = edge_list[0]
+        source_node_id = edge_list[1]
+        destination_node_id = edge_list[-1]
+        duration = int(edge_list[3])
+        edge = Edge(destination_node_id, duration)
+        if edge_list[4] == ":":
+            edge.prob = int(edge_list[5])
+        return (graph_id, source_node_id, edge)
+    
+"""
+The main shell
+"""
+class GraaShell(cmd.Cmd):
+    intro = 'Type help or ? to list commands.\n'
+    prompt = 'graa> '
+    def __init__(self):
+        self.session = GraaSession()
+        self.parser = GraaParser()
+        self.scheduler = GraaScheduler(self.session)
+        super().__init__()
+    def do_tempo(self, arg):
+        'set beat tempo'
+        try:
+            self.session.tempo = int(arg)
+            print("set tempo to {} bpm".format(self.session.tempo))
+        except ValueError:
+            print("invalid tempo specification")
+    def do_print(self, arg):
+        'print specified digraph to file'
+        self.session.graphs[arg].render("graph_" + arg, "comment")
+        print('tbd')
+    def do_quit(self, arg):
+        'Quit graa.'
+        print("quitting graa on next beat")
+        self.session.active = False
+        self.scheduler.beat_thread.join()
+        for player_key in self.session.players.keys():
+            player = self.session.players[player_key]
+            player.active = False
+            player.graph_thread.join()
+        print("Quitting, bye!")
+        return True
+    def do_q(self, arg):
+        'Quit graa.'
+        return self.do_quit(arg)
+    def do_play(self, arg):
+        'Play graph'
+        self.scheduler.queue_graph(arg)
+    def default(self, arg):
+        # ignore comment lines
+        if arg[0] == "#":
+            return False
+        # this should be done better somehow
+        try:
+            self.session.add_node(self.parser.parse_node(arg))
+            # print(node_results)
+        except ParseException:
+            #print(str(Error))
+            try:
+                self.session.add_edge(self.parser.parse_edge(arg))
+                # print(edge_results)
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                print("invalid input!")
+                raise
+        
+if __name__ == '__main__':
+    shell = GraaShell()
+    shell.cmdloop()
+    
