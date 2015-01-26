5baff20ab57cf81a920bc922b6a47410626128aa
nik <nik@parkellipsen.de>
2015-01-27 00:07:01 +0100

    put tbs back

--- graa.py
+++ graa.py
@@ -2,5 +2,33 @@ from graa_generators import *
 from graa_language_functions import *
 
 
+# IDEAS:
+# learning: store paths, rate performances, automatically play on that basis ? 
+# centralized clock for collaborative graaing ?
+# node locking mode ? (give nodes a duration ?)
+# configurable resolution for non-realtime systems ? how, if it's strongly timed ... :(
+
+# TO BE DONE:
+# tbd: make everything more robust (changing graphs during playing etc ...)
+# tbd: resetting graphs
+# tbd: validation: all nodes reachable etc ?
+# tbd: re-sync graphs on beat (restart command ?)
+# tbd: graphs containing graphs, for longer compositions !
+# tbd: edge probability modification 
+# tbd: edge duration modification
+# tbd: peristent modifications ?
+# tbd: supercollider backend
+# tbd: multiple edges at once: b1-500->b2-500->b3 ?
+# tbd: edge rebalancing (subtract equally from existing edges if not enough prob left)
+# tbd: documentation
+# tbd: play modes: markov, manual, beat (?)
+# tbd: manual timeshift: d1@+/-x
+# tbd: multi-command lines
+# tbd: add overlay kwargs if not present
+# tbd: mute nodes through overlays
+# tbd: two overlay modes d <<plus>> ol: non-persistent, only apply functions, d <<permaplus>> ol: store values
+# tbd: graph generators: tree, triforce, bjoerklund, cross, etc (see notes)
+# tbd: write graph transformers, like, reverse, tournament, rotate (?) 
+
 
     
