7089c0bff65a8b4420da809df74c89e0c85503c0
nik <nik@parkellipsen.de>
2015-02-15 01:51:40 +0100

    updated tbds

--- graa.py
+++ graa.py
@@ -7,28 +7,23 @@ from graa_language_functions import *
 # node locking mode ? (give nodes a duration ?)
 # configurable resolution for non-realtime systems ? how, if it's strongly timed ... :(
 
-# TO BE DONE:
+# TO BE DONE (for next releases ...)
 # tbd: add overlay kwargs if not present (in doubt on default value)
 # tbd: avoid namespace conflicts between sound- and overlay functions ...
 # tbd: avoid "hidden edges" ... ?
 # tbd: more ChucK backend functions
-# tbd: electric qoutes (emacs mode)
 # tbd: write abstractions / generators for common functions
-# tbd: rock-solid division between graphs and overlays
-# tbd: template edges for generators, to avoid name collisions ...
 # tbd: emacs expand for moar lines !
-# tbd: MOAR yasnippets for functions
-# tbd: selctively expand edges and nodes ??
-# tbd: precise scheduling ...
+# tbd: more precise scheduling ... ?
 # tbd: validation: all nodes reachable etc ?
 # tbd: re-sync graphs on beat (restart command ?)
 # tbd: graphs containing graphs, for longer compositions ?
 # tbd: edge probability modification
 # tbd: edge rebalancing (subtract equally from existing edges if not enough prob left) (ESP for edge mods!!)
 # tbd: documentation
-# tbd: play modes: markov, manual, beat (?)
 # tbd: graph generators: grid, tree, triforce, bjoerklund, etc (see notes)
 # tbd: write graph transformers, like, reverse, tournament, rotate (?) 
-
+# tbd: performance recorder ?
+# tbd: configurable default start mode ...
 
     
